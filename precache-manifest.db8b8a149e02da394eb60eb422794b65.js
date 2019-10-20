200cc510c4ad24aacc51ad80f7efedba6d6aeb81
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2019-10-19 21:07:22 -0300

    Updates

---
+++ precache-manifest.db8b8a149e02da394eb60eb422794b65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9aedc12d2a910ff3fbb41544b920c07",
+    "url": "/react-lottery-app/index.html"
+  },
+  {
+    "revision": "cbd095b327e50f49fd88",
+    "url": "/react-lottery-app/static/css/main.d4ad182b.chunk.css"
+  },
+  {
+    "revision": "8137af9b03adbcf92a2a",
+    "url": "/react-lottery-app/static/js/2.eb60ce64.chunk.js"
+  },
+  {
+    "revision": "cbd095b327e50f49fd88",
+    "url": "/react-lottery-app/static/js/main.569f4941.chunk.js"
+  },
+  {
+    "revision": "92d6efea86b8f1cfed81",
+    "url": "/react-lottery-app/static/js/runtime-main.401f144b.js"
+  }
+]);
